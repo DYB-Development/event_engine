14c94ba0baf4ac29bc743a5fc46a36d12185e62a
tylercschneider <tylercschneider@gmail.com>
2026-06-08 16:16:45 -0400

    Serialize process_type in Schema#to_ruby

--- lib/event_engine/event_definition/schemas.rb
+++ lib/event_engine/event_definition/schemas.rb
@@ -41,6 +41,7 @@ module EventEngine
               event_version: #{event_version.inspect},
               event_type: #{event_type.inspect},
               event_level: #{event_level.inspect},
+              process_type: #{process_type.inspect},
               required_inputs: #{required_inputs.inspect},
               optional_inputs: #{optional_inputs.inspect},
               payload_fields: [#{payload_fields.map { |h| ruby_hash(h) }.join(", ")}]
