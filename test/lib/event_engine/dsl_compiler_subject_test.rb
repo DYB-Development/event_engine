5d691a93f86de171d705bb071eec9bfbc801bc12
tylercschneider <tylercschneider@gmail.com>
2026-06-19 16:33:43 -0400

    Reject unregistered subjects at compile time

---
+++ test/lib/event_engine/dsl_compiler_subject_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class DslCompilerSubjectTest < ActiveSupport::TestCase
+  def teardown
+    EventEngine.reset_subjects!
+  end
+
+  test "compile rejects a definition whose subject is not registered" do
+    definition = Class.new(EventEngine::EventDefinition) do
+      event_name :processed
+      event_type :domain
+      subject :unregistered
+    end
+
+    assert_raises(EventEngine::SubjectRegistry::UnknownSubjectError) do
+      EventEngine::DslCompiler.compile([definition])
+    end
+  end
+end
