a98664933b4145bc984b2909d06d10ddbdff0daf
tylercschneider <tylercschneider@gmail.com>
2026-06-19 22:46:10 -0400

    Merge default metadata envelope onto emitted events

---
+++ test/lib/event_engine/metadata_enricher_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+require "ostruct"
+
+module EventEngine
+  class MetadataEnricherTest < ActiveSupport::TestCase
+    include EventEngineTestHelpers
+
+    class CowFed < EventDefinition
+      event_name :cow_fed
+      event_type :domain
+
+      input :cow
+      required_payload :weight, from: :cow, attr: :weight
+    end
+
+    setup do
+      @helpers_snapshot = snapshot_event_engine_helpers
+
+      compiled = DslCompiler.compile([CowFed])
+      compiled.finalize!
+
+      event_schema = EventSchema.new
+      compiled.events.each do |event|
+        schema = compiled.latest_for(event).dup
+        schema.event_version = 1
+        event_schema.register(schema)
+      end
+      event_schema.finalize!
+
+      registry = SchemaRegistry.new
+      registry.reset!
+      registry.load_from_schema!(event_schema)
+
+      EventEngine.install_helpers(registry: registry)
+    end
+
+    teardown do
+      restore_event_engine_helpers(@helpers_snapshot)
+      EventEngine.configuration.metadata_defaults = nil
+    end
+
+    test "emitted event carries the default metadata envelope" do
+      EventEngine.configuration.metadata_defaults = -> { { app_version: "1.0" } }
+
+      event = EventEngine.cow_fed(cow: OpenStruct.new(weight: 500))
+
+      assert_equal "1.0", event.metadata[:app_version]
+    end
+  end
+end
