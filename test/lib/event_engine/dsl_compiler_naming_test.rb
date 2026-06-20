c4b2a06286ebf64e6d6eb62745176cfecb463ec2
tylercschneider <tylercschneider@gmail.com>
2026-06-19 23:23:05 -0400

    Report all invalid event names in one compile error

--- test/lib/event_engine/dsl_compiler_naming_test.rb
+++ test/lib/event_engine/dsl_compiler_naming_test.rb
@@ -11,4 +11,21 @@ class DslCompilerNamingTest < ActiveSupport::TestCase
       EventEngine::DslCompiler.compile([definition])
     end
   end
+
+  test "compile reports every invalid event name in one error" do
+    camel = Class.new(EventEngine::EventDefinition) do
+      event_name :CowFed
+      event_type :domain
+    end
+    dotted = Class.new(EventEngine::EventDefinition) do
+      event_name :"cow.fed"
+      event_type :domain
+    end
+
+    error = assert_raises(EventEngine::DslCompiler::InvalidEventNameError) do
+      EventEngine::DslCompiler.compile([camel, dotted])
+    end
+
+    assert_includes error.message, "cow.fed"
+  end
 end
