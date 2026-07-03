d22198ad23634c52fff08b290dbab81e8e63cb82
tylercschneider <tylercschneider@gmail.com>
2026-07-02 23:21:28 -0400

    Add EventSchemaJsonLoader to build a registry from the JSON artifact

---
+++ test/lib/event_engine/event_schema_json_loader_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+require "tempfile"
+require "json"
+
+class EventSchemaJsonLoaderTest < ActiveSupport::TestCase
+  def schema_h(event_name:, version:)
+    EventEngine::EventDefinition::Schema.new(
+      event_name: event_name,
+      event_version: version,
+      event_type: :domain,
+      required_inputs: [:cow],
+      optional_inputs: [],
+      payload_fields: [{ name: :weight, required: true, from: :cow, attr: :weight }]
+    ).to_h
+  end
+
+  def write_json(*schemas)
+    file = Tempfile.new(["event_schema", ".json"])
+    file.write(JSON.pretty_generate(schemas))
+    file.close
+    file
+  end
+
+  test "loads every event and version from the JSON artifact" do
+    file = write_json(
+      schema_h(event_name: :cow_fed, version: 1),
+      schema_h(event_name: :cow_fed, version: 2)
+    )
+
+    registry = EventEngine::EventSchemaJsonLoader.load(file.path)
+
+    assert_equal [1, 2], registry.versions_for(:cow_fed)
+  ensure
+    file.unlink
+  end
+end
